4c0feacc18057876cf9a8488f281af0d047dfbfb
Stacy Faizulina <stacy.faizulina@shopify.com>
2022-06-26 21:56:55 +0100

    get real data from query indtead of fixtures

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,9 +1,12 @@
 import Query from "./resolvers/Query"
-import Db from "./db"
+import Db, { DbTweet, DbUser } from "./db"
 import { Resolvers } from "./resolvers-types.generated"
 
 export interface TwitterResolverContext {
-    db: Db
+    db: Db,
+    dbTweetCache: Record<string, DbTweet>,
+    dbTweetToFavouriteCountMap: Record<string, number>,
+    dbUserCache: Record<string, DbUser>,
   }
 
 const resolvers: Resolvers<TwitterResolverContext> = {
